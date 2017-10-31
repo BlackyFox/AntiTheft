67ff064a78fdb74d3f1c41dc4c7c22fc15a20dfb
itr007 <itr007@passi.lan>
2017-10-31 16:18:19 +0100

    Adding fragments

--- app/src/main/java/blackyfox/fr/antitheft/MainActivity.java
+++ app/src/main/java/blackyfox/fr/antitheft/MainActivity.java
@@ -1,5 +1,10 @@
 package blackyfox.fr.antitheft;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.annotation.ColorRes;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,6 +15,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Fragment frag = null;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,25 +24,53 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
+                    frag = MenuFragment.newInstance(getString(R.string.title_home),getColorFromRes(R.color.colorPrimary));
+                    if (frag != null) {
+                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                        ft.add(R.id.container, frag, frag.getTag());
+                        ft.commit();
+                    }
                     return true;
                 case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
+                    //mTextMessage.setText(R.string.title_settings);
+                    frag = MenuFragment.newInstance(getString(R.string.title_settings),getColorFromRes(R.color.colorPrimary));
+                    if (frag != null) {
+                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                        ft.add(R.id.container, frag, frag.getTag());
+                        ft.commit();
+                    }
                     return true;
                 case R.id.navigation_infos:
-                    mTextMessage.setText(R.string.title_infos);
+                    //mTextMessage.setText(R.string.title_infos);
+                    frag = MenuFragment.newInstance(getString(R.string.title_infos),getColorFromRes(R.color.colorPrimary));
+                    if (frag != null) {
+                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                        ft.add(R.id.container, frag, frag.getTag());
+                        ft.commit();
+                    }
                     return true;
             }
             return false;
         }
     };
 
+    private int getColorFromRes(@ColorRes int resId) {
+        return ContextCompat.getColor(this, resId);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        frag = MenuFragment.newInstance(getString(R.string.title_home),getColorFromRes(R.color.colorPrimary));
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
